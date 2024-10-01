16f01ef30eefb2d5c5623865bdb06a6d87904741
brettp02 <brettpenwarden@gmail.com>
2024-10-02 12:27:09 +1300

    Setup TestGetLogs, working on specific tests now

--- src/test/java/nz/ac/wgtn/swen301/server/TestGetLogs.java
+++ src/test/java/nz/ac/wgtn/swen301/server/TestGetLogs.java
@@ -1,4 +1,26 @@
 package nz.ac.wgtn.swen301.server;
 
+import org.junit.jupiter.api.*;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
 public class TestGetLogs {
+    private LogsServlet logsServlet;
+    private MockHttpServletResponse response;
+    private MockHttpServletRequest request;
+
+
+    @BeforeEach
+    public void setup() {
+        logsServlet = new LogsServlet();
+        response = new MockHttpServletResponse();
+        request = new MockHttpServletRequest();
+
+        // Clear DB before each test
+        Persistency.DB.clear();
+    }
+
 }
